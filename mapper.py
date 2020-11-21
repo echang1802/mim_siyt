0020655445f8f634e67c2c8f4c93781e5518e549
echang1802 <41762304+echang1802@users.noreply.github.com>
2020-11-21 10:44:45 -0300

    TP2

---
+++ mapper.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from pyarrow import parquet
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+
+def category_from_filename(filename):
+    """
+    Extracts the category from the filename, e.g. if filename is MLA1055_reviews_page0.parquet it should return MLA1055
+
+    :param filename: name of the file
+    :return: extracted category
+    """
+    # TODO: implementar
+    extension = filename.split("MLA")[1]
+    return "MLA" + extension[:4]
+
+
+class Review:
+    """
+    Models an Item's review.
+    """
+    def __init__(self, key, category, title, content, rate, likes, dislikes):
+        self.key = key
+        self.category = category
+        self.title = title
+        self.content = content
+        self.rate = rate
+        self.likes = likes
+        self.dislikes = dislikes
+
+    @classmethod
+    def from_record(cls, record, filename):
+        """
+        Constructs a Review from the contents of a record and the filename it belongs to
+
+        :param record: Series with the attributes of a Review
+        :param filename: file to which the record belongs to
+        :return: an instance of Review
+        """
+        category = category_from_filename(filename)
+        # TODO: reemplazar o ajustar el resto de este método para que extraiga las características del review
+        #       según los nombres de columnas que hay en sus archivos parquet
+        return Review(
+            key=record['review_key'],
+            category=category,
+            title=record['review_title'],
+            content=record['review_content'],
+            rate=record['review_rate'],
+            likes=record['review_likes'],
+            dislikes=record['review_dislikes']
+        )
+
+
+def accept(term):
+    """
+    Acceptance criteria for a term
+
+    :param term: term to accept or not
+    :return: True if the term is accepted, False otherwise
+    """
+    # TODO implementar acá un filtro que devuelve True sólo para las palabras de interés,
+    #      por ejemplo, eliminando STOPWORDS o filtrando sólo palabras alfanuméricas.
+    return (not term in stopwords.words('spanish')) & term.isalnum()
+
+
+def sanitize(word):
+    """
+    Cleans and normalizes the word
+
+    :param word: word to sanitize
+    :return: sanitized word
+    """
+    # TODO: normalizar la palabra aquí, por ejemplo pasar a minúsculas y eliminar espacios extra
+    word = word.lower()
+    replacement = {
+        " " : "",
+        "á" : "a",
+        "é" : "e",
+        "í" : "i",
+        "ó" : "o",
+        "ú" : "u"
+    }
+    for key, value in replacement.items():
+        word = word.replace(key, value)
+    return word
+
+
+def get_score_bucket(score, threshold=3):
+    """
+    Binarizes the score into "negative" and "positive" buckets
+
+    :param score: score to binarize
+    :param threshold: threshold below which the score is considered negative and above positive
+    :return: the bucket
+    """
+    if score <= threshold:
+        return "negative"
+    else:
+        return "positive"
+
+
+def map_function(content, category, bucket):
+    """
+    Map part of MapReduce
+
+    :param content: the content of a review
+    :param category: the category to which the review belongs
+    :param bucket: the bucket to which the review belongs ("negative" or "positive")
+    :return: a list of (key, value) pairs to be shuffled and reduced later on.
+    """
+    out = []
+    for word in word_tokenize(content):
+        term = sanitize(word)
+        if not accept(term):
+            continue
+        key = term + "\t" + category + "\t" + bucket
+        value = 1
+        out.append((key, value))
+
+    return out
+
+
+def main():
+    output = []
+    for inputfile in sys.argv[1:]:
+        records = parquet.read_pandas(inputfile).to_pandas()
+        for index, record in records.iterrows():
+            review = Review.from_record(record, os.path.basename(inputfile))
+            output.extend(map_function(review.content, review.category, get_score_bucket(review.rate)))
+
+    # The (key, value) pairs are output in sorted order by key
+    for key, value in sorted(output):
+        print(key + "\t" + str(value))
+
+
+if __name__ == '__main__':
+    main()
---
+++ reducer.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import csv
+import math
+import sys
+
+
+def reduce_function(buffer, totals):
+    """
+    Reduce part of MapReduce
+
+    :param buffer: list of (key, value) to reduce (here key is formed by the term, category and bucket).
+    :param totals: dictionary where the reduced key,values are collected
+    :return: nothing
+    """
+    # TODO aquí deben recorrer los términos, categorías, buckets y valores que hay en el buffer
+    #       y por cada uno de ellos actualizar el diccionario totals.
+    #       totals es un diccionario donde la clave es cada bucket, y el valor es un diccionario
+    #       de categorías a un diccionario de términos y cantidades:
+    #       totals[bucket][category][term] está asociado a un número entero
+    for (term, category, bucket, value) in buffer:
+        if not bucket in totals.keys():
+            totals[bucket] = {}
+        if not category in totals[bucket].keys():
+            totals[bucket][category] = {}
+        totals[bucket][category][term] = value
+
+def calculate_tfs(totals):
+    """
+    Calculates the term frequencies and creates inverse indexes mapping terms to buckets and to categories
+
+    :param totals: Reduced key,values
+    :return: a tuple consisting of (term frequencies, map of term:buckets, map of term:categories)
+    """
+    tfs = {}
+    term_buckets = {}
+    term_categories = {}
+
+    for bucket, bucket_totals in totals.items():
+        tfs[bucket] = {}
+        for category, category_totals in bucket_totals.items():
+            tfs[bucket][category] = {}
+            for term, frequency in category_totals.items():
+                tfs[bucket][category][term] = frequency / sum(category_totals.values())
+
+                if term not in term_buckets:
+                    term_buckets[term] = set()
+                term_buckets[term].add(bucket)
+
+                if term not in term_categories:
+                    term_categories[term] = set()
+                term_categories[term].add(category)
+
+    return tfs, term_buckets, term_categories
+
+
+def calculate_tfs_idfs(tfs, term_buckets, term_categories):
+    """
+    Calculates a modified version of TF-IDF that better suits our problem
+
+    :param tfs: term frequencies
+    :param term_buckets: map term:buckets
+    :param term_categories: map term:categories
+    :return: the modified TF-IDF scores of each term per (category, bucket)
+    """
+    tf_idfs = {}
+    n_buckets = len(tfs)
+    n_categories = len(list(tfs.values())[0])
+    for bucket, bucket_tfs in tfs.items():
+        tf_idfs[bucket] = {}
+        for category, category_tfs in bucket_tfs.items():
+            tf_idfs[bucket][category] = {}
+            for term, tf in category_tfs.items():
+                # TODO aquí deben calcular el tfs/idfs del bucket, categoría y término y volcarlo en el diccionario tf_idfs
+                idf = math.log(n_catgories / term_categories[term].len())
+                tf_idfs[bucket][category][term] = tf * idf
+
+    return tf_idfs
+
+
+def merge(totals):
+    """
+    Merge function to be applied after reducing all key,values
+
+    :param totals: reduced key,values
+    :return: the result of merging all the reduced keys
+    """
+    tfs, term_buckets, term_categories = calculate_tfs(totals)
+
+    return calculate_tfs_idfs(tfs, term_buckets, term_categories)
+
+
+def write_csv(category, bucket, sorted_scores):
+    """
+    Writes the terms and TF-IDF scores corresponding to a (category, bucket) to a csv file.
+
+    :param category: category
+    :param bucket: bucket ("negative" or "positive")
+    :param sorted_scores: list of (term, score) sorted by score from highest to lowest
+    """
+    with open("terms_" + category + "_" + bucket + ".csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(('Término', 'TF/IDF'))
+        writer.writerows(sorted_scores)
+
+
+def main():
+    totals = {}
+    cur_key = None
+    buffer = []
+
+    for input_line in sys.stdin:
+        term, category, bucket, value = input_line.strip().split("\t")
+        if cur_key == (term, category):
+            buffer.append((term, category, bucket, int(value)))
+        else:
+            cur_key = (term, category)
+            if buffer:
+                reduce_function(buffer, totals)
+                buffer = []
+            else:
+                buffer.append((term, category, bucket, int(value)))
+
+    tf_idfs = merge(totals)
+
+    for bucket, bucket_tf_idfs in tf_idfs.items():
+        for category, category_tf_idfs in bucket_tf_idfs.items():
+            sorted_scores = sorted(category_tf_idfs.items(), key=lambda x: x[1], reverse=True)
+            write_csv(category, bucket, sorted_scores)
+
+
+
+if __name__ == '__main__':
+    main()
